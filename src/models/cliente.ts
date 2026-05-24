8fa0bed700daed0c4a18e00bd165d0c8c0f2910f
Anninhastar23 <camposannalivia127@gmail.com>
2026-05-23 23:36:18 -0300

    Modificada a classe Cliente e criada as classes vendedor e estoque

--- src/models/cliente.ts
+++ src/models/cliente.ts
@@ -1,12 +1,12 @@
 class cliente {
-    id: number
+    id_cliente: number
     nome: string
     cpf: string
     email?: string
     cidade?: string
 
     constructor(nome: string, cpf: string, telefone: string, email: string, cidade: string) {
-        this.id = this.geraID()
+        this.id_cliente = this.geraID()
         this.nome = nome
         this.cpf = cpf
         this.email = email
---
+++ src/models/vendedor.ts
@@ -0,0 +1,16 @@
+class vendedor{
+    id_vendedor: number
+    nome: string
+    matricula: string
+    comissao_percentual: number
+
+    constructor(nome: string, matricula: string, comissao_percentual: number) {
+        this.id_vendedor = this.geraID()
+        this.nome = nome
+        this.matricula = matricula
+        this.comissao_percentual = comissao_percentual
+    }
+    private geraID(): number {
+        return Date.now()
+    }
+}
