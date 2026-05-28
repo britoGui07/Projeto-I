06f2f7a2bc9c78e0dd010b00cc268c485366db11
britoGui07 <andradebritoguilherme@gmail.com>
2026-05-28 00:58:22 -0300

    Repository de Carro finalizado

--- src/repositories/carroRepository.ts
+++ src/repositories/carroRepository.ts
@@ -1,8 +1,8 @@
-import {Carro} from "../models/carro"
+import {Carro, carroData} from "../models/carro"
 
 let carros: Carro[] = []
 
-export function novoCarro(data: Omit<Carro, 'id_carro'> ): Carro{
+export function novoCarro(data: carroData): Carro{
     const novoCarro = new Carro(data.marca, data.modelo, data.ano, data.placa, data.preco, data.cor)
     carros.push(novoCarro)
     return novoCarro
@@ -20,8 +20,16 @@ export function buscarPelaPlaca(placa: string): Carro | undefined{
     return carros.find(c => c.placa === placa)
 }
 
-export function atualizarCarro(id: number, data: Partial<Omit<Carro, 'id_carro'>>): Carro | undefined{
-    const pos_carro = carros.findIndex(c => c.id_carro === id)
+export function atualizarCarro(id: number, data: carroData): Carro | undefined{
+    const carro = carros.find(c => c.id_carro === id)
+    if(!carro) return undefined
 
-    return carros[pos_carro]
+    if(data.marca) carro.marca = data.marca
+    if(data.modelo) carro.modelo = data.modelo
+    if(data.ano) carro.ano = data.ano
+    if(data.placa) carro.placa = data.placa
+    if(data.preco) carro.preco = data.preco
+    if(data.cor) carro.cor = data.cor
+    
+    return carro
 }
