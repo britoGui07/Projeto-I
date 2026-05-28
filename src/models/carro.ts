c27d31e56c1635f418419ecb075951b73cac833d
britoGui07 <andradebritoguilherme@gmail.com>
2026-05-28 00:57:57 -0300

    Model de carro atualizado com adição da interface carroData

--- src/models/carro.ts
+++ src/models/carro.ts
@@ -1,20 +1,24 @@
-export class Carro{
-    id_carro: number
+export interface carroData{
     marca: string
     modelo: string
     ano: number
     placa: string
     preco: number
     cor: string
+}
 
-    constructor(marca: string, modelo: string, ano: number, placa: string, preco: number, cor: string){
+export class Carro{
+    id_carro: number
+
+    constructor(
+        public marca: string, 
+        public modelo: string, 
+        public ano: number, 
+        public placa: string, 
+        public preco: number, 
+        public cor: string
+    ){
         this.id_carro = this.geraId()
-        this.marca = marca
-        this.modelo = modelo
-        this.ano = ano
-        this.placa = placa
-        this.preco = preco
-        this.cor = cor
     }
 
     private geraId(): number{
