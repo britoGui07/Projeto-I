a2dd9874dc9729ad37e201bb31e6335104695b43
Anninhastar23 <camposannalivia127@gmail.com>
2026-05-29 15:23:59 -0300

    criação do repository de clientes + adição de interfaces no models de cliente e vendedor

--- src/models/carro.ts
+++ src/models/carro.ts
@@ -10,17 +10,18 @@ export interface carroData{
 export class Carro{
     id_carro: number
 
-    constructor(marca: string, modelo: string, ano: number, placa: string, preco: number, cor: string){
+    constructor(
+        public marca: string, 
+        public modelo: string, 
+        public ano: number, 
+        public placa: string, 
+        public preco: number, 
+        public cor: string
+    ){
         this.id_carro = this.geraId()
-        this.marca = marca
-        this.modelo = modelo
-        this.ano = ano
-        this.placa = placa
-        this.preco = preco
-        this.cor = cor
     }
 
-    private geraID(): number{
+    private geraId(): number{
         return Date.now()
     }
 }
